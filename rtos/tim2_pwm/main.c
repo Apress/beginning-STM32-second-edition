e0d48cf9aeb2cdcebe925423c9962babf5d700d7
Warren Gay <ve3wwg@gmail.com>
2023-11-22 10:56:59 -0500

    libopencm3 API change

--- rtos/tim2_pwm/main.c
+++ rtos/tim2_pwm/main.c
@@ -72,7 +72,7 @@ task1(void *args __attribute__((unused))) {
 int
 main(void) {
 
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
